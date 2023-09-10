7b4c1758a5db876247650fbacea4c062d9a291d3
MoeOnGit <malrobiay04@gmail.com>
2023-09-10 01:03:19 -0600

    init

---
+++ Lab 1/hello.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main() {
+
+	printf("Nice to meet you CMPUT 201!\n");
+	return 0;
+
+}
---
+++ Lab 1/hello.py
@@ -0,0 +1,7 @@
+import sys
+
+def main():
+	
+	print("Nice to meet you CMPUT 201!")
+
+main()
