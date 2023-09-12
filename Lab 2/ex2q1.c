c037779814f536414c0f882a83e743ee1b387f8e
moealrobiay <mohammedalrobiay12@gmail.com>
2023-09-12 12:14:08 -0600

    init

--- Lab 2/ex2q1.c
+++ Lab 2/ex2q1.c
@@ -2,21 +2,37 @@
 #include <stdlib.h>
 #include <limits.h>
 
-int main() {// This program finds the maximum element in an array
-	int length, index = 0;
-	if (scanf("%d", &length) == 1) {
+int main() {
+    int length, index = 0;
+    
+    // Prompt the user for the array length
+    printf("Enter the length of the array: ");
+    if (scanf("%d", &length) == 1) {
         if (length <= 0) {
             printf("Invalid array length\n");
             return 1;
         }
-        int array[length];
-		for (int i = 0; i < length; i++) {
-			if (scanf("%d", &array[i]) != 1)
-				exit(100);
-			if (array[index] < array[i])
-				index = i;
-		}
-		printf("The maximum element is %d\n", array[index]);
-	}
-	return 0;
+        
+        int *array = (int *)malloc(length * sizeof(int));
+        if (array == NULL) {
+            printf("Memory allocation failed\n");
+            return 1;
+        }
+        
+        // Prompt the user for each array element
+        for (int i = 0; i < length; i++) {
+            printf("Enter element %d: ", i + 1);
+            if (scanf("%d", &array[i]) != 1)
+                exit(100);
+            if (array[index] < array[i])
+                index = i;
+        }
+        
+        printf("The maximum element is %d\n", array[index]);
+        
+        // Don't forget to free the allocated memory when you're done.
+        free(array);
+    }
+    
+    return 0;
 }
