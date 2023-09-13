20fcb045d8a233de9ece4fee39c8f26ec4b2fdf6
moealrobiay <mohammedalrobiay12@gmail.com>
2023-09-13 12:37:51 -0600

    init

--- Lab 2/ex2q1.c
+++ Lab 2/ex2q1.c
@@ -4,8 +4,12 @@
 
 int main() {// This program finds the maximum element in an array
     int length, index = 0;
+
+    printf("Enter the length of the array: ");
     if (scanf("%d", &length) == 1) {
         int array[length];
+
+        printf("Enter the array elements: \n");
         for (int i = 0; i < length; i++) {
             if (scanf("%d", &array[i]) != 1)
                 exit(100);
@@ -16,5 +20,4 @@ int main() {// This program finds the maximum element in an array
 
     }
     return 0;
-
 }
