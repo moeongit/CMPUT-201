3bbe2f11eebaeba2f5d073ad17020b9b6752e11d
moealrobiay <mohammedalrobiay12@gmail.com>
2023-09-14 11:43:37 -0600

    init

--- Lab 2/ex2q2.c
+++ Lab 2/ex2q2.c
@@ -1,32 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
-int main(){
-
+int main() {
     int Cad;
 
     printf("Enter CAD Amount: ");
-    if (scanf("%d", &Cad) != 1 || Cad < 0 || Cad >= 100000){
+    if (scanf("%d", &Cad) != 1 || Cad < 0 || Cad >= 100000) {
         printf("Please enter a Positive Integer less than 100,000.\n");
         return 1;
     }
 
-    int USD = (Cad * 0.75);
+    double USD = Cad * 0.75; 
 
-    printf("Converted USD Amount: $%d\n", USD);
-    printf("$100 bills: %d\n", USD/100);
-    USD %= 100;
-    printf("$50 bills: %d\n", USD/50);
-    USD %= 50;
-    printf("$20 bills: %d\n", USD/20);
-    USD %= 20;
-    printf("$10 bills: %d\n", USD/10);
-    USD %= 10;
-    printf("$5 bills: %d\n", USD/5);
-    USD %= 5;
-    printf("$2 bills: %d\n", USD/2);
-    USD %= 2;
-    printf("$1 bills: %d\n", USD);
-    
+    int roundedUSD = round(USD);
+
+    printf("Converted USD Amount: $%d\n", roundedUSD);
+    printf("$100 bills: %d\n", roundedUSD / 100);
+    roundedUSD %= 100;
+    printf("$50 bills: %d\n", roundedUSD / 50);
+    roundedUSD %= 50;
+    printf("$20 bills: %d\n", roundedUSD / 20);
+    roundedUSD %= 20;
+    printf("$10 bills: %d\n", roundedUSD / 10);
+    roundedUSD %= 10;
+    printf("$5 bills: %d\n", roundedUSD / 5);
+    roundedUSD %= 5;
+    printf("$2 bills: %d\n", roundedUSD / 2);
+    roundedUSD %= 2;
+    printf("$1 bills: %d\n", roundedUSD);
+
     return 0;
 }
