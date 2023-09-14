3a599fe050eedf8f25acb692c1f9d14c3e069653
moealrobiay <mohammedalrobiay12@gmail.com>
2023-09-14 15:51:46 -0600

    init

--- Lab 2/ex2q2.c
+++ Lab 2/ex2q2.c
@@ -1,38 +1,25 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 int main() {
-    int Cad;
-
-    printf("Enter CAD Amount: ");
-    if (scanf("%d", &Cad) != 1 || Cad < 0 || Cad >= 100000) {
-        printf("Please enter a Positive Integer less than 100,000.\n");
+    int cadAmount;
+    
+    printf("Enter CAD amount: ");
+    if (scanf("%d", &cadAmount) != 1 || cadAmount < 0 || cadAmount >= 100000) {
+        printf("Invalid input\n");
         return 1;
     }
-
-    double USD = Cad * 0.75; 
-    double round = USD - (int)USD;
     
-    if (round >= 0.5) {
-        USD = (int)USD + 1.0;
-    } else {
-        USD = (int)USD;
+    int usdAmount = (int)((cadAmount * 0.75) + 0.5); // Convert CAD to USD and round to nearest USD
+    int usdBills[] = {100, 50, 20, 10, 5, 2, 1};
+    int usdCount[7] = {0};
+    
+    printf("Converted USD amount: $%d\n", usdAmount);
+    
+    for (int i = 0; i < 7; i++) {
+        usdCount[i] = usdAmount / usdBills[i];
+        usdAmount %= usdBills[i];
+        printf("$%d bills: %d\n", usdBills[i], usdCount[i]);
     }
-
-    printf("Converted USD Amount: $%.0f\n", USD);  
-    printf("$100 bills: %d\n", (int)(USD / 100));
-    USD -= (int)(USD / 100) * 100;
-    printf("$50 bills: %d\n", (int)(USD / 50));
-    USD -= (int)(USD / 50) * 50;
-    printf("$20 bills: %d\n", (int)(USD / 20));
-    USD -= (int)(USD / 20) * 20;
-    printf("$10 bills: %d\n", (int)(USD / 10));
-    USD -= (int)(USD / 10) * 10;
-    printf("$5 bills: %d\n", (int)(USD / 5));
-    USD -= (int)(USD / 5) * 5;
-    printf("$2 bills: %d\n", (int)(USD / 2));
-    USD -= (int)(USD / 2) * 2;
-    printf("$1 bills: %d\n", (int)USD);
-
+    
     return 0;
 }
