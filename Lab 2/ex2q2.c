1c97cc911ffa55772edbf780261a823f6cc6a799
MoeOnGit <malrobiay04@gmail.com>
2023-09-14 18:47:57 -0600

    init

--- Lab 2/ex2q2.c
+++ Lab 2/ex2q2.c
@@ -5,22 +5,26 @@ int main() {
     
     printf("Enter CAD amount: ");
     if (scanf("%d", &CAD) != 1 || CAD < 0 || CAD >= 100000) {
-        printf("Invalid input. Enter a non-negative Integer.\n");
+        printf("Invalid input. Please enter a Non-Negative Integer.\n");
         return 1;
     }
     
-    int usdAmount = (int)((CAD * 0.75) + 0.5); 
-    int usdBills[] = {100, 50, 20, 10, 5, 2, 1};
-    int usdCount[7] = {0}; // This array is so we can have the amount 
-                          // of each type of bill written in the for loop
+    int USD = (int)((CAD * 0.75) + 0.5); 
     
-    printf("Converted USD amount: $%d\n", usdAmount);
-    
-    for (int i = 0; i < 7; i++) {
-        usdCount[i] = usdAmount / usdBills[i];
-        usdAmount %= usdBills[i];
-        printf("$%d bills: %d\n", usdBills[i], usdCount[i]);
-    }
-    
+    printf("Converted USD amount: $%d\n", USD);
+    printf("$100 bills: %d\n", USD / 100);
+    USD %= 100;
+    printf(" $50 bills: %d\n", USD / 50);
+    USD %= 50;
+    printf(" $20 bills: %d\n", USD / 20);
+    USD %= 20;
+    printf(" $10 bills: %d\n", USD / 10);
+    USD %= 10;
+    printf("  $5 bills: %d\n", USD / 5);
+    USD %= 5;
+    printf("  $2 bills: %d\n", USD / 2);
+    USD %= 2;
+    printf("  $1 bills: %d\n", USD);
+
     return 0;
 }
