c0d3d60db940f1f29edede4280b4aab28ddf5794
moealrobiay <mohammedalrobiay12@gmail.com>
2023-09-15 12:47:01 -0600

    init

--- Lecture Examples/class5.c
+++ Lecture Examples/class5.c
@@ -1,10 +1,27 @@
 #include <stdio.h>
 
-int main(){
+int main(int argc, char **argv){
 
-    int x, y, j, k;
+    int max = 0;
+    int n = 10;
+    int a[10] = {7, 5, 6, 8, 3, 2};
 
-    
+    printf("The storage size of variable 'max' is : %lu\n", sizeof(max));
+    printf("The length of an array 'a' is: %d\n", n);
+    printf("The storage size of array 'a' is : %lu\n", sizeof(a));
+
+    for (int i = 0; i < n; i++){
+
+        if (max < a[i]){
+            max = a[i];
+        }
+
+    }
+
+
+    printf("The maximum Integer is: %d\n", max);
+
+
 
 
 
