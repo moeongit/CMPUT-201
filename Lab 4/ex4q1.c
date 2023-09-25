70b3dbd672703ebb8065e155b5973bae4f23d9f4
moealrobiay <mohammedalrobiay12@gmail.com>
2023-09-25 12:42:10 -0600

    init

--- Lab 4/ex4q1.c
+++ Lab 4/ex4q1.c
@@ -1,1 +1,24 @@
 #include <stdio.h>
+
+int main(){
+
+    double SalePrice[] = {0.50, 1.25, 3.00, 1.00};
+    double PurchasePrice[] = {0.05, 0.25, 0.70, 0.30};
+
+    while (){
+        switch(c){
+            case c:
+            
+            case t:
+
+            case p:
+
+            case l:
+
+
+        }
+    }
+
+
+
+}
