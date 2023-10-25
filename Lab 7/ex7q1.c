99d782ad51486c5189a374082191b3edd61167f7
MoeOnGit <malrobiay04@gmail.com>
2023-10-25 15:38:27 -0600

    init

--- Lab 7/ex7q1.c
+++ Lab 7/ex7q1.c
@@ -2,58 +2,60 @@
 #include <stdlib.h>
 
 int main() {
-    int numAisles, serialCode;
+    int NumOfAisles, SerialNum;
     char product, **aisles;
 
-    scanf("%d", &numAisles);
+    scanf("%d", &NumOfAisles);
 
-    aisles = (char**)malloc(numAisles * sizeof(char*));
-    int* aisleLengths = (int*)malloc(numAisles * sizeof(int));
+    aisles = (char**)malloc(NumOfAisles * sizeof(char*));
+    int* aisleLengths = (int*)malloc(NumOfAisles * sizeof(int));
 
-    for(int i = 0; i < numAisles; i++) {
+    for(int i = 0; i < NumOfAisles; i++) {
         aisles[i] = NULL;
         aisleLengths[i] = 0;
     }
 
     while (1) {
-        int readValue;
-        if (scanf("%d", &readValue) == EOF) { 
+        int ValueNum;
+        if (scanf("%d", &ValueNum) == EOF) { 
             break;
         }
 
-        if (readValue == 0) {
+        if (ValueNum == 0) {
             int index;
+
             scanf("%d", &index);
 
             if (aisles[index]) {
                 printf("%s\n", aisles[index]);
+                
             } else {
                 printf("\n");
             }
         } else {
-            serialCode = readValue; 
-
+            SerialNum = ValueNum; 
             getchar(); 
             product = getchar();
 
-            int index = serialCode % numAisles;
+            int index = SerialNum % NumOfAisles;
             int len = aisleLengths[index];
 
             aisles[index] = (char*)realloc(aisles[index], (len + 2) * sizeof(char));
             aisles[index][len] = product;
             aisles[index][len + 1] = '\0';
 
-            aisleLengths[index]++; // Increment the length after adding a product
+            aisleLengths[index]++; 
         }
     }
 
-    // Free allocated memory.
-    for(int i = 0; i < numAisles; i++) {
+    for(int i = 0; i < NumOfAisles; i++) {
         if(aisles[i]) {
             free(aisles[i]);
         }
     }
+
     free(aisles);
+
     free(aisleLengths);
 
     return 0;
