bf836e437f785d902beda68f4a99ca31851a4e12
MoeOnGit <malrobiay04@gmail.com>
2023-11-02 00:51:12 -0600

    init

--- Lab 8/ex8q1.c
+++ Lab 8/ex8q1.c
@@ -1,15 +1,79 @@
 #include "ex8q1.h"
 
+struct Entry {
+    float index;
+    int value;
+    struct Entry* next;
+};
+
+Entry* insert_entry(Entry* head, float index, int value) {
+    Entry* NewEntry = malloc(sizeof(Entry));
+    NewEntry->index = index;
+    NewEntry->value = value;
+    NewEntry->next = NULL;
+    
+    if (!head) {
+        return NewEntry;
+    }
+
+    if (head->index > index) {
+        NewEntry->next = head;
+        return NewEntry;
+    }
+
+    Entry* current = head;
+    while (current->next && current->next->index < index) {
+        current = current->next;
+    }
+
+    if (current->next && current->next->index == index) {
+        current->next->value = value;
+        free(NewEntry);
+    } 
+    else {
+        NewEntry->next = current->next;
+        current->next = NewEntry;
+    }
+
+    return head;
+}
+
+void print_array(Entry* head) {
+    printf("[");
+    Entry* current = head;
+    while (current) {
+        printf("%d", current->value);
+        if (current->next){
+            printf(", ");
+        } 
+        current = current->next;
+    }
+    printf("]\n");
+}
+
+void free_array(Entry* head) {
+    while (head) {
+        Entry* temp = head;
+        head = head->next;
+        free(temp);
+    }
+}
+
 int main(void) {
     int is_print, value;
     float index;
+    Entry* ArrayHead = NULL;
 
     while (!feof(stdin)) {
-        // Use the read_line function from ex7q1.h here! Do not try to parse the
-        // input yourself (that's a lot harder)
-
-        // read_line(...);
-
-        fprintf(stderr, "%d %0.4f %d\n", is_print, index, value);  // You can remove this
+        read_line(&is_print, &index, &value);
+        if (is_print) {
+            print_array(ArrayHead);
+        } 
+        else {
+            ArrayHead = insert_entry(ArrayHead, index, value);
+        }
     }
+
+    free_array(ArrayHead);
+    return 0;
 }
